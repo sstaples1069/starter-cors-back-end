5839c67e651b32c5ea1123301c2e2728ef29c9f3
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 14:24:00 -0800

    completed doThis and fully functional

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,17 +1,28 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+//^1 when i want on specific methods: GET, POST, PUT, etc
+//^1 const corsDelete = cors({methods: "DELETE"});
+
+//^A when used for entire router
+//^A router.use(cors())
 
 router
   .route("/:corsId")
+  //^R when used for all methods on a particular route
+  //^R .all(cors())
   .get(controller.read)
   .put(controller.update)
+  //^1 .delete(corsDelete, controller.delete)
+  //^1 .options(corsDelete)
   .delete(controller.delete)
   .all(methodNotAllowed);
 
 router
   .route("/")
-  .get(controller.list)
+  .get(cors(), controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
 
